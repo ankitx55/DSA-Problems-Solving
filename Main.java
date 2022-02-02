25f19d08d29d1d5f9e51d837b1bbdb7bdf6ffccf
ankitx55 <ankitx55@gmail.com>
2022-02-02 19:10:55 +0530

    new changes to the files

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args)
     {
         int key = 123;
-        long inte = key;
+        long integer = key;
+        System.out.println(key);
     }
 }
