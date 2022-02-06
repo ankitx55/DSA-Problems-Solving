bcc03c43551decda1b15cf3e0b62e40047437f1a
ankitx55 <ankitx55@gmail.com>
2022-02-07 02:56:58 +0530

    New changes to the remove Element file

--- RemoveElement.java
+++ RemoveElement.java
@@ -17,29 +17,42 @@ public class RemoveElement {
 
     static int removeElement(int[] nums, int val) {
 
-        int count = 0;
-        int length;
-        for (length = nums.length-1; length > 0; length--) {
-            for (int i = 0; i < length; i++) {
+
+//         swapping all the target elements to the last possible index
+//         using the code of selection sort
+
+        for(int length=nums.length-1; length>0; length--)
+        {
+            for(int i = 0; i<length; i++)
+            {
+//                 if arr[i] equals target it should be
+//                 swapped to the last element through this.
                 if(nums[i]==val)
                     swap(nums, i, length);
-                else if(nums[i]!=val)
-                {count++;}
             }
         }
-        // int k = nums.length-count;
-        int k = count;
-        return k;
+
+//         to count the array length
+        int count = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i] != val)
+                count++;
+        }
+
+        return count;
+
     }
 
-    static void swap(int nums[], int i, int j)
+    //     to swap the target element with last element
+    static void swap(int[] nums, int i, int j)
     {
-        int temp=0;
-        temp = nums[i];
+        int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-        return;
     }
+
+//     Okay Bye!!
 
 
 }
