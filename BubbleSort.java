a669f8e4d91fdb56cf44520c45fc17c3f47ce6b6
Ankit <ankit@Ankits-MacBook-Pro.local>
2023-01-07 00:04:38 +0530

    new changes

---
+++ BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+        int l=arr.length;
+        BubbleSortAlgorithm(arr, l);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void BubbleSortAlgorithm(int arr[], int l){
+        for(int i=l-1; i>0; i--){
+            for(int j=0; j<l-1; j++){
+                int temp;
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
