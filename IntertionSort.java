39cd3cc70806eb8d271a4d8f0e902088d993ec9b
Ankit <ankit@Ankits-MacBook-Pro.local>
2023-01-12 21:03:35 +0530

    new file added

---
+++ IntertionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class IntertionSort {
+    public static void main(String[] args){
+        int arr[] = {6,5,4,3,3,2,1, -111, 4444};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for(int j=i+1; j>0; j--){
+//                if current element is smaller than the previous one then swap
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+//                if current element is not smaller then break out of the loop
+                else
+                    break;
+            }
+        }
+    }
+
+}
