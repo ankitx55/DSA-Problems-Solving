bbf4e249170ef71e492beba31eaf10a50366a338
Ankit <ankit@Ankits-MacBook-Pro.local>
2023-01-20 13:26:42 +0530

    new changes

--- CyclicSort.java
+++ CyclicSort.java
@@ -2,24 +2,24 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] arr = {6,5,4,3,2,1};
+        int[] arr = {3,5,2,1,4};
         cyclic(arr);
         System.out.println(Arrays.toString(arr));
-
     }
 
     static void cyclic(int[] arr){
-        int j=0;
+        int a = 0;
+
         for (int i = 0; i < arr.length; i++) {
-            int currElement = arr[j];
-            if(currElement==arr[currElement-1]){
-                
+            int correctIndex = arr[a]-1;
+            if (arr[a] != arr[correctIndex]){
+                int temp =arr[a];
+                arr[a]=arr[correctIndex];
+                arr[correctIndex]=temp;
             }
             else{
-                int temp = currElement;
-                currElement=arr[currElement-1];
-                arr[currElement-1]=temp;
-            }
+                a++;
             }
         }
     }
+}
