0715217b9e8aec7402d8cc512996288dfb8912d7
Kit <ankitx55@gmail.com>
2023-09-11 09:04:00 +0530

    new file

---
+++ square_root.java
@@ -0,0 +1,19 @@
+public class square_root {
+//    https://leetcode.com/problems/sqrtx/
+// We have to find the square root of a number but the catch is not to use any operator or inbuilt function
+public static void main(String[] args) {
+    int num = 25;
+    System.out.println("hello");
+}
+
+//static int sq_root(int num){
+////    base condition
+//    if(num==1){
+//        return num;
+//    }
+//    generate recursive function
+    
+//    return the recursive function
+
+
+}
